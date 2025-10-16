7553a89a5937a3d6de6342cd629f82100523b989
rorena15 <rorena1586@gmail.com>
2025-10-16 20:33:07 +0900

    change locate

--- php/log_ransomware.php
+++ php/log_ransomware.php
@@ -2,5 +2,5 @@
 session_start();
 $data = json_decode(file_get_contents('php://input'), true);
 $logLine = date('Y-m-d H:i:s') . " - Session: " . session_id() . " - " . $data['event'] . "\n";
-file_put_contents(dirname(__FILE__) . '/../../log.txt', $logLine, FILE_APPEND | LOCK_EX);
+file_put_contents(dirname(__FILE__) . '/../log.txt', $logLine, FILE_APPEND | LOCK_EX);
 echo 'Logged';
